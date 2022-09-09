a3165d93c0ddd9a762bbda1582cb168480f5bfea
IronBoneZ <noppakhun.phae@bumail.net>
2022-09-09 21:32:56 +0700

    Code Update

---
+++ Teleport.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject txtToDisplay;
+
+    private bool PlayerInZone;
+
+    public GameObject Player;
+
+    public GameObject TPLoc;
+    
+    [SerializeField] private float delay = 3f;
+
+    private float timeElapse;
+
+    private void Start()
+    {
+
+        PlayerInZone = false;     
+        txtToDisplay.SetActive(false);
+    }
+
+    private void Update()
+    {
+        timeElapse += Time.deltaTime;
+        
+        if (PlayerInZone && Input.GetKeyDown(KeyCode.E))           //if in zone and press F key
+        {
+            
+            StartCoroutine(DelayTeleport());
+            
+        }
+    }
+
+     private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")     //if player in zone
+        {
+            txtToDisplay.SetActive(true);
+            PlayerInZone = true;
+        }
+     }
+    
+
+    private void OnTriggerExit(Collider other)     //if player exit zone
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            PlayerInZone = false;
+            txtToDisplay.SetActive(false);
+        }
+    }
+
+    IEnumerator DelayTeleport()
+    {
+        yield return new WaitForSeconds(2f); // Wait 1 seconds
+        gameObject.GetComponent<AudioSource>().Play();
+            Player.transform.position = TPLoc.transform.position;
+    }
+}
